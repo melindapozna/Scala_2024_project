e520edb8a3c8a5782d6e812c3b803238aceb14bb
Melinda Pozna <melindapozna@gmail.com>
2024-05-06 10:20:20 +0300

    produce energy and write produced energy to a file

--- src/main/scala/plants/Plant.scala
+++ src/main/scala/plants/Plant.scala
@@ -5,9 +5,18 @@ import java.io.File
 trait Plant {
     val name: String
 
-    //def getData(): List[String]
-    //def produceEnergy(): List[String]
-    //def writeProducedEnergyToFile(file: File): Unit
+    //sets the multiplier when calculating the produced energy
+    var workPercentage: Double
+    def setWorkPercentage(x: Double): Unit = {
+        this.workPercentage = x
+    }
+    //TODO implement
+    //def getData(): List[Double]
+    def produceEnergy(data: List[Double]): List[Double] = {
+            data.map(_ * workPercentage)
+    }
+
+    def writeProducedEnergyToFile(data: List[Double]): Unit
     def isMalfunctioning(): Boolean = {
         // to simulate malfunctioning, it generates a random number and checks if it's equal to 0
         val rand: Random = new Random()
@@ -16,7 +25,4 @@ trait Plant {
         }
         false
     }
-
-
-
 }
--- src/main/scala/plants/SolarPlant.scala
+++ src/main/scala/plants/SolarPlant.scala
@@ -1,6 +1,15 @@
 package plants
+import java.io._
 
 class SolarPlant extends Plant {
     override val name = "Solar panel"
+    override var workPercentage = 0.1
+
+    override def writeProducedEnergyToFile(data: List[Double]): Unit = {
+        val dataOutputStream = new DataOutputStream(new FileOutputStream(new File("solar.txt")))
+        data.foreach(dataOutputStream.writeDouble)
+        dataOutputStream.close()
+    }
+
 
 }
